1dc1dd9d7a644c2c2f724be2cace02a95de17acf
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-01-17 08:59:36 +0530

    Update app_theme.dart

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -30,22 +30,38 @@ class AppTheme {
     return themeData.formUiHelper;
   }
 
-// Method to set custom colors for the app theme
-// static void setThemeColors({
-//   Color? primaryColor,
-//   Color? secondaryColor,
-//   Color? backgroundColor,
-//   Color? buttonColor,
-//   Color? textColor,
-//   Color? appBarColor,
-//   Color? borderColor,
-// }) {
-//   if (primaryColor != null) themeColor.primaryColor = primaryColor;
-//   if (secondaryColor != null) themeColor.secondaryColor = secondaryColor;
-//   if (backgroundColor != null) themeColor.backgroundColor = backgroundColor;
-//   if (buttonColor != null) themeColor.buttonColor = buttonColor;
-//   if (textColor != null) themeColor.textColor = textColor;
-//   if (appBarColor != null) themeColor.appBarColor = appBarColor;
-//   if (borderColor != null) themeColor.borderColor = borderColor;
-// }
+  // Method to set custom colors for the app theme
+  static void setThemeColors({
+    Color? primary,
+    Color? secondary,
+    Color? tertiary,
+    Color? inverse,
+    Color? text,
+    Color? secondaryText,
+    Color? success,
+    Color? warning,
+    Color? dimGray,
+    Color? darkGray,
+    Color? lightBlue,
+    Color? purple,
+    Color? dimBlack,
+    Color? appBarColor,
+    Color? borderColor,
+  }) {
+    if (primary != null) themeData.colors.primary = primary;
+    if (secondary != null) themeColor.secondary = secondary;
+    if (tertiary != null) themeColor.tertiary = tertiary;
+    if (inverse != null) themeColor.inverse = inverse;
+    if (text != null) themeColor.text = text;
+    if (secondaryText != null) themeColor.secondaryText = secondaryText;
+    if (success != null) themeColor.success = success;
+    if (warning != null) themeColor.warning = warning;
+    if (dimGray != null) themeColor.dimGray = dimGray;
+    if (darkGray != null) themeColor.darkGray = darkGray;
+    if (lightBlue != null) themeColor.lightBlue = lightBlue;
+    if (purple != null) themeColor.purple = purple;
+    if (dimBlack != null) themeColor.dimBlack = dimBlack;
+    if (appBarColor != null) themeColor.appBarColor = appBarColor;
+    if (borderColor != null) themeColor.borderColor = borderColor;
+  }
 }
